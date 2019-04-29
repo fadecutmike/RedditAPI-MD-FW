52aeef13a8eba0911072cacaf2d1e7fa004a32dd
Mike Dimore <mikeoct13@gmail.com>
2019-04-29 07:12:55 -0400

    Initial Commit

---
+++ mdimore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mdimore
+//
+//  Created by Michael Dimore on 4/29/19.
+//  Copyright © 2019 Michael Dimore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
